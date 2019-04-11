d2d07504190d4e0a1965f2e34f97827f4cc8a8e9
Amal Jose <amal@dexlock.com>
2019-04-11 10:05:33 +0530

    routing

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService,public messageService: MessageService) { 
+    
+  }
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+}
